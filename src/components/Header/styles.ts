e6dc68c0005614b6d91e7269ce6e83205bbce680
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2020-05-01 02:11:17 -0400

    First commit

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -22,6 +22,7 @@ export const Container = styled.div<ContainerProps>`
         text-decoration: none;
         font-size: 16px;
         transition: opacity 0.2s;
+        opacity: 0.6;
 
         & + a {
           margin-left: 32px;
@@ -29,6 +30,12 @@ export const Container = styled.div<ContainerProps>`
 
         &:hover {
           opacity: 0.6;
+        }
+
+        &.active {
+          opacity: 1;
+          border-bottom: 2px solid #ff872c;
+          padding-bottom: 10px;
         }
       }
     }
